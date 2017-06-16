7d5d5cbd494fad21b7c42e4d999980cbe293aa86
Andrian Motelica <a.motelica@mail.ru>
2017-06-16 10:08:03 +0300

    php env runtime correction

--- phpEnv/56/runtime/run.sh
+++ phpEnv/56/runtime/run.sh
@@ -6,5 +6,8 @@ service apache2 restart
 # setup permissions
 chmod -R 777 /var/www/html
 
+#create env symlink
+ln -s /var/sym/.env /var/www/html/project/.env
+
 # run to infinity
 tail -f /dev/null
